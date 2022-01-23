05d71ea41e930657728f14bff0e06b41c53fedc7
kkouis <kkouis@sqli.com>
2022-01-23 19:04:43 +0100

    Add enum typeCompte

--- src/main/java/com/craftsman/craftsman/entites/Compte.java
+++ src/main/java/com/craftsman/craftsman/entites/Compte.java
@@ -1,5 +1,7 @@
 package main.java.com.craftsman.craftsman.entites;
 
+import main.java.com.craftsman.craftsman.enums.TypeCompte;
+
 import java.util.Date;
 
 public class Compte {
@@ -7,9 +9,9 @@ public class Compte {
     private Long code;
     private double solde;
     private Date dateCreation;
-    private String type;
+    private TypeCompte type;
 
-    public Compte(Long code, double solde, Date dateCreation, String type) {
+    public Compte(Long code, double solde, Date dateCreation, TypeCompte type) {
         this.code = code;
         this.solde = solde;
         this.dateCreation = dateCreation;
@@ -24,11 +26,27 @@ public class Compte {
         return dateCreation;
     }
 
-    public String getType() {
+    public TypeCompte getType() {
         return type;
     }
 
     public Long getCode() {
         return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public void setType(TypeCompte type) {
+        this.type = type;
     }
 }
---
+++ src/main/java/com/craftsman/craftsman/enums/TypeCompte.java
@@ -0,0 +1,4 @@
+package main.java.com.craftsman.craftsman.enums;
+
+public enum TypeCompte {
+}
