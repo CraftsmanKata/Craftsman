700118d9b8291e5fe51a8115a60512cb1b8e7446
CraftsmanKata <98129570+CraftsmanKata@users.noreply.github.com>
2022-01-24 10:25:14 +0100

    Update ExempleUn.java

--- src/test/java/com/craftsman/craftsman/tdd/ExempleUn.java
+++ src/test/java/com/craftsman/craftsman/tdd/ExempleUn.java
@@ -9,7 +9,7 @@ public class ExempleUn {
 
     @Test
     void TestGetTypeCompte(){
-        Compte compte = new Compte(null,12000,new Date(), "COURANT");
+        Compte compte = new Compte(null,12000,new Date(), TypeCompte.COURANT);
         Assertions.assertEquals(compte.getType(),"COURANT");
     }
 
