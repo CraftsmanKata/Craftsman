1e1e82c0c1934f037d017f266d4d9746fcba4d50
kkouis <kkouis@sqli.com>
2022-02-02 12:16:30 +0100

    update

--- src/test/java/com/craftsman/craftsman/tdd/ExempleUn.java
+++ src/test/java/com/craftsman/craftsman/tdd/ExempleUn.java
@@ -11,7 +11,7 @@ public class ExempleUn {
     @Test
     void TestGetTypeCompte(){
         Compte compte = new Compte(null,12000,new Date(), TypeCompte.COURANT);
-        Assertions.assertEquals(compte.getType(),"COURANT");
+        Assertions.assertEquals(compte.getType().toString(),"COURANT");
     }
 
     @Test
